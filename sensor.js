fb1f7512fc8f554ed7cdf48a0e45ba3df32b9e0e
Steven LIU <steven0127@gmail.com>
2020-04-04 20:40:17 +0800

    bug fix6

--- sensor.js
+++ sensor.js
@@ -1,22 +1,22 @@
 var onoff = require('onoff');
 
 var Gpio = onoff.Gpio, 
-    led = new Gpio(4, 'out'),
+    //led = new Gpio(4, 'out'),
     sensor = new Gpio(15, 'in', 'both');
 
 sensor.watch(function(err, value){
     if(err) exit(err);
     console.log(value ?'I got light!' :'I lost all light...');
-    led.write(value ?1 :0);
+    //led.write(value ?1 :0);
 });
 
 function exit(err){
     if (err) console.log('An error occurred: '+err);
     sensor.unexport();
-    led.writeSync(0);
-    led.unexport();
+    //led.writeSync(0);
+    //led.unexport();
     console.log('Bye then...');
     process.exit();
 }
 
-process.on('SIGINT', exit());
+process.on('SIGINT', exit);
