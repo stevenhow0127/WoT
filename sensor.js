097387c3dd800fb6f85a5e537a0d6566c0365987
Steven LIU <steven0127@gmail.com>
2020-04-04 20:19:58 +0800

    sensor first commit

---
+++ sensor.js
@@ -0,0 +1,22 @@
+var onoff = require('onoff');
+
+var Gpio = onoff.Gpio, 
+    led = new Gpio(4, 'out'),
+    sensor = new Gpio(15, 'in', 'both');
+
+sensor.watch(function(err, value){
+    if(err) exit(err);
+    console.log(value ?'I got light!' :'I lost all light...');
+    led.writeSync(value ?1 :0);
+});
+
+function exit(err){
+    if (err) console.log('An error occurred: '+err);
+    sensor.export();
+    led.writeSync(0);
+    led.export();
+    console.log('Bye then...');
+    process.exit();
+}
+
+process.on('SIGINT', exit);
